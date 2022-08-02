687b8671a8754f96a1a284fa68b546f499d7dad5
shin <sssin9163@gmail.com>
2022-08-02 21:20:46 +0900

    apply metrial ui

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
 import React,{ Component} from 'react';
 import './App.css';
 import Customer from './components/Customer';
+import Paper from '@mui/material/Paper';
+import Table from '@mui/material/Table';
+import TableHead from '@mui/material/TableHead';
+import TableBody from '@mui/material/TableBody';
+import TableRow from '@mui/material/TableRow';
+import TableCell from '@mui/material/TableCell';
 
-const customer=[{
+
+
+
+
+const customers=[{
   'id':1,
   'image':'http://placeimg.com/64/64/1',
   'name':'홍길동',
@@ -29,26 +39,43 @@ const customer=[{
 ]
 class  App extends Component {  
   render(){
+    const {classes} = this.props;
+
     return (
-       <div>
-        {
-          customer.map(c=>{
-             return(
-              <Customer
-                  key={c.id}
-                  id={c.id}
-                  image={c.image}
-                  name={c.name}
-                  birthday={c.birthday}
-                  gender={c.gender}
-                  job={c.job}/>
-             )
-          })
-        }
-         
-        </div>
+       <Paper >
+
+           <Table >
+              <TableHead>
+                <TableRow>
+                  <TableCell>번호</TableCell>
+                  <TableCell>이미지</TableCell>
+                  <TableCell>이름</TableCell>
+                  <TableCell>생일</TableCell>
+                  <TableCell>성별</TableCell>
+                  <TableCell>직업</TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {customers.map(c=>{ return(
+                                          <Customer
+                                              key={c.id}
+                                              id={c.id}
+                                              image={c.image}
+                                              name={c.name}
+                                              birthday={c.birthday}
+                                              gender={c.gender}
+                                              job={c.job}
+                                            />           
+                                          )          
+                                    }
+                                )
+                }
+              </TableBody>
+            </Table>
+
+          </Paper>
     )
   };  
 }
 
-export default App;
+export default  App;
--- src/components/Customer.js
+++ src/components/Customer.js
@@ -1,37 +1,21 @@
+import { TableRow } from '@mui/material';
 import React from 'react';
+import TableCell from '@mui/material/TableCell';
 
 
 class Customer extends  React.Component{
     render(){
         return(
-            <div>
-                <CustomerProfile id={this.props.id} image={this.props.image} name={this.props.name}/>
-                <CustomerInfo name={this.props.name} birthday={this.props.birthday} job={this.props.job}  />
-            </div>
-        )
-    }
-}
-
-class CustomerProfile extends React.Component{
-    render(){
-        return(
-            <div>
-                
-                <img src={this.props.image} alt='profile' />
-                <h2>{this.props.name}({this.props.id})</h2>
-            </div>
-        )
-    }
-}
-
-class CustomerInfo extends React.Component{
-    render(){
-        return(
-            <div>
-               <p>{this.props.name}</p>
-               <p>{this.props.birthday}</p>
-               <p>{this.props.job}</p>
-            </div>
+            
+                <TableRow>
+                    <TableCell> {this.props.id}</TableCell>
+                    <TableCell><img src={this.props.image} alt='profile'/> </TableCell>
+                    <TableCell> {this.props.name}</TableCell>
+                    <TableCell> {this.props.birthday}</TableCell>
+                    <TableCell> {this.props.gender}</TableCell>
+                    <TableCell> {this.props.job}</TableCell>                 
+                </TableRow>                
+           
         )
     }
 }
